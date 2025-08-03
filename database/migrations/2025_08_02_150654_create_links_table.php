d27888cce642285589c55395917bcd6a54dbbadd
Silvio Martins <silviosmsantos@gmail.com>
2025-08-03 17:25:40 -0300

    [feature]: creating relationship on migration

--- database/migrations/2025_08_02_150654_create_links_table.php
+++ database/migrations/2025_08_02_150654_create_links_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->require();
             $table->string('link')->require();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
