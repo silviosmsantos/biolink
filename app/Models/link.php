70d101d29207661e222d71c116a5634cf1f723e8
Silvio Martins <silviosmsantos@gmail.com>
2025-08-11 21:03:47 -0300

    [refact]: refactoring moveUp and moveDown methods

--- app/Models/link.php
+++ app/Models/link.php
@@ -12,8 +12,44 @@ class Link extends Model
     /** @use HasFactory<\Database\Factories\LinkFactory> */
     use HasFactory;
 
-    public function users()
+    public function user()
     {
         return $this->BelongsTo(User::class);
+    }
+
+    public function moveUp()
+    {
+        $this->move(-1);
+    }
+
+    public function moveDown()
+    {
+        $this->move(+1);
+    }
+
+
+    /**
+     * Swap the order of the link with the link $to positions above/below it.
+     *
+     * @param int $to The number of positions to move the link. Negative to move up, positive to move down.
+     *
+     * @return void
+     */
+    private function move(int $offset): void 
+    {
+         $this->load('user'); 
+
+        if (!$this->user) {
+            return; 
+        }
+
+        $swapLink = $this->user->links()
+            ->where('order', $this->order + $offset)
+            ->first();
+
+        if ($swapLink) {
+            $swapLink->fill(['order' => $this->order])->save();
+            $this->fill(['order' => $this->order + $offset])->save();
+        }
     }
 }
