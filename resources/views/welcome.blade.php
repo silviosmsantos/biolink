b54ccedbf869dd9f6d1d1f8fda6de3bb4f9b2ad2
Silvio Martins <silviosmsantos@gmail.com>
2025-09-11 22:03:10 -0300

    [chore]: daisyUI install

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang=" {{ config('app.locale') }} ">
+<html lang=" {{ config('app.locale') }} " class="h-full">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +7,8 @@
     @vite('resources/css/app.css')
     <title>{{ config('app.name') }}</title>
 </head>
-<body>
-    <h1 class="text-3xl font-bold underline">
+<body class="bg-slate-100 h-full"></body>
+    <h1 class="text-3xl font-bold underline text-cyan-700">
       Hello world!
     </h1>
 </body>
