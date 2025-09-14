f870386af70f0ac693324f2a2ca1169c940b085c
Silvio Martins <silviosmsantos@gmail.com>
2025-09-14 10:28:33 -0300

    [feature]: creating blade componente - layout page

---
+++ resources/views/components/layout/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang=" {{ config('app.locale') }} " class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>{{ config('app.name') }}</title>
+</head>
+<body class="bg-slate-800 h-full"></body>
+    {{ $slot }}
+</body>
+</html>
